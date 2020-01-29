f0643f734511ca4cf62201c5e3d6a2264102db2c
Fitri Rozi <fitrirozi1@gmail.com>
2020-01-29 14:13:46 -0600

    Updates

---
+++ precache-manifest.bac79ed4c0d5655cc17070ea48585f24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30c33e8a545a9b1d8844afe977989297",
+    "url": "/pokemon/index.html"
+  },
+  {
+    "revision": "1e4a4a49082f5b9c8a9d",
+    "url": "/pokemon/static/css/main.201b31b6.chunk.css"
+  },
+  {
+    "revision": "5f58cb42b27c9adeb5cd",
+    "url": "/pokemon/static/js/2.981df369.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/pokemon/static/js/2.981df369.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1e4a4a49082f5b9c8a9d",
+    "url": "/pokemon/static/js/main.507d66b5.chunk.js"
+  },
+  {
+    "revision": "302210217b0687d9e548",
+    "url": "/pokemon/static/js/runtime-main.c3b45077.js"
+  }
+]);
